416b470739df5c6cded4d9015b44e7f63e443b88
Vaishnav Pawar <128246169+vai4507@users.noreply.github.com>
2023-10-15 00:15:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AgeCalculator from './Agecalculator';
+
+function App() {
+  return (
+    <div className="App">
+      <AgeCalculator />
+    </div>
+  );
+}
+
+export default App;
